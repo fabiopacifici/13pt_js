0f07d12f5384840cb475a5217a37e4fd9dff605c
fabiopacifici <boolean@fabiopacifici.com>
2026-03-14 12:04:42 +0100

    Enhance DOM manipulation examples with styling and event listeners

--- 07_dom_manipulation/main.js
+++ 07_dom_manipulation/main.js
@@ -79,13 +79,123 @@ console.log(pEl.classList);
 console.log(pEl.className)
 // update the classes 
 //pEl.className = pEl.className + ' fabio'
-pEl.className +=' fabio'
+pEl.className += ' fabio'
 
 console.log(pEl.className)
 
 
 // Style
+// read the style object
+console.log(pEl.style);
+// read the single property
+console.log(textElement.style.backgroundColor); // red
+textElement.style.color = 'white'
+textElement.style.padding = '10px'
 
+textElement.style.backgroundColor = 'purple'
+
+
+// Read  HTML attributes via js
+const imgElement = document.querySelector('img')
+console.log(imgElement.src); // https://picsum.photos/400/200
+
+// write
+imgElement.alt = 'A random image of a dog'
+
+containerEl.append('Fabio')
+containerEl.innerHTML += 'Ciao'
+containerEl.textContent += 'Ciao'
+//containerEl.appendChild('<p>ciao</p>')
+
+// DOM API
+const paragphEl = '<p>ciao</p>'
+console.log(paragphEl);
+
+const paragraphDOM = document.createElement('p')
+console.log(paragraphDOM.style.color);
+paragraphDOM.style.color = 'red'
+
+
+paragraphDOM.classList.add('p-5', 'bg-dark')
+paragraphDOM.innerText = 'Ciao ciao';
+console.log(paragraphDOM);
+
+
+containerEl.appendChild(paragraphDOM)
+
+// DOM Template literals
+
+const title = 'Learn DOM Manupulation'
+const content = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae, adipisci?'
+const markupString = `  
+<div class="card">
+    <div class="card-body">
+      <h3>${title}</h3>
+      <p>${content}</p>
+    </div>
+  </div>`
+
+console.log(typeof markupString); // string
+console.log(markupString);
+
+
+
+
+// Mix the two approaches
+const cardBodyMarkupString = `  
+
+    <div class="card-body">
+      <h3>${title}</h3>
+      <p>${content}</p>
+    </div>
+ `
+
+console.log(cardBodyMarkupString);
+
+const cardDOM = document.createElement('div')
+cardDOM.classList.add('card')
+console.log(cardDOM);
+
+cardDOM.innerHTML = cardBodyMarkupString
+console.log(cardDOM);
 
 // Event listeners
+// 1️⃣ select the dom node where attach the listener
+const buttonEl = document.querySelector('button')
 
+// 2️⃣ Attache the 'click' event
+buttonEl.addEventListener('click', function () {
+  // do your things here
+  console.log('Clicked the button');
+
+})
+
+// with arrow function
+imgElement.addEventListener('click', () => {
+  console.log('Clicked with arrow');
+
+})
+
+// with a function handler
+
+function handleImageClick() {
+  console.log('You clicked the second button');
+
+}
+
+const btnEl = document.getElementById('btn')
+// this syntax works only when the function has no parameters
+btnEl.addEventListener('click', handleImageClick)
+
+
+// with a function with params
+
+function greetUser(name) {
+  console.log(`Ciao ${name}`);
+
+}
+
+const btnGreetUserEl = document.getElementById('greetUser')
+btnGreetUserEl.addEventListener('click', function () {
+  greetUser('Fabio')
+})
